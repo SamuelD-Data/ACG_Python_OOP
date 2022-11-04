efaffaaf17351a10fdc1d61812620fbff1ba44d8
Samuel Davila <68249338+SamuelD-Data@users.noreply.github.com>
2022-11-04 14:25:14 -0500

    Update essay_question.py

--- chapter_4/essay_question.py
+++ chapter_4/essay_question.py
@@ -1,15 +1,24 @@
+# creating EssayQuestion class
+# question_text is set as an attribute while answer
+# and answer_is_sufficient are class variables set to none and False, respectively
 class EssayQuestion:
   def __init__(self, question_text) -> None:
       self.question_text = question_text
       self.answer = None
       self.answer_is_sufficient = False
 
+# str method is set to return question_text as a string      
   def __str__(self) -> str:
       return self.question_text
 
+# returning answer as str
   def select(self, answer) -> None:
       self.answer = str(answer)
 
+# printing question_text, 2 line breaks then answer
+# sufficient class variable asks for input as to whether answer is sufficient as y or n
+# sets sufficient class variable true if input value starts with a "y", otherwise false
+# returns if answer is sufficient as True or False
   def grade(self) -> bool:
       print(f"{self.question_text}\n\nAnswer:\n{self.answer}\n")
       sufficient = input("Is this answer sufficient? [Yn]: ")
